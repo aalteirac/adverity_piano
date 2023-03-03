e9a8573b65188b4990f947b0a3a30076237cbdaf
Anthony Alteirac <105443206+sfc-gh-aalteirac@users.noreply.github.com>
2023-03-03 15:54:35 +0100

    ico on vide tab

--- campaign3.py
+++ campaign3.py
@@ -33,7 +33,7 @@ def getCard(text,val,icon, compare=False):
     if compare==False:
         pgcol='darkgrey'
     style={'icon': icon,'icon_color':'#535353','progress_color':pgcol}
-    icoSize="20vw"
+    icoSize="16vw"
     if compare==False:
         return hc.info_card(key=key,title=val, title_text_size="16vw",content=str(text),content_text_size="10vw",icon_size=icoSize,theme_override=style)
     else:
