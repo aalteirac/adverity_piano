0cd46e3585136e31cd391112293effff57e9c27f
Anthony Alteirac <105443206+sfc-gh-aalteirac@users.noreply.github.com>
2023-03-06 12:57:23 +0100

    perf

--- whatif.py
+++ whatif.py
@@ -56,6 +56,10 @@ def getCampaignSelectionBox(map,selec):
         'Remove Campaign to estimate saving:',
     map['CAMPAIGN'].unique(),default=selec.sort_values(['CAMPAIGN'])['CAMPAIGN'].unique(),key='campaign')
 
+def reset():
+    st.session_state['reset']=True
+
+
 def getPage(sess):
     global session 
     session = sess
@@ -63,35 +67,43 @@ def getPage(sess):
     orig=dt.copy()
     countries=st.session_state.get('country') 
     campaings=st.session_state.get('campaign') 
+    allCountries=orig['COUNTRY_NAME'].unique()
+    allCampaign=orig['CAMPAIGN'].unique() 
+    
+
     clusterSelected=st.session_state.get('clusterstore')
-    kmeans = KMeans(init="random", n_clusters=5, n_init=10, random_state=1)
     if st.session_state.get('clusNum') is not None:
         kmeans = KMeans(init="random", n_clusters=st.session_state.get('clusNum'), n_init=10, random_state=1)
+    else:
+        kmeans = KMeans(init="random", n_clusters=5, n_init=10, random_state=1)
     clusterDF=orig.groupby(['COUNTRY_NAME']).agg({
                                     'CTR':"mean",
                                     'ER':'mean',
                                     'VIDEO_COMPLETION_RATE':'mean'
                                     }).reset_index()
     kmeans.fit(clusterDF[['CTR','ER','VIDEO_COMPLETION_RATE']])  
+
     clusterDF['VIDEO_COMPLETION_RATE']=clusterDF['VIDEO_COMPLETION_RATE']*100
     clusterDF['CLUSTER'] = kmeans.labels_ 
-    # clusterDF['CLUSTER']  = clusterDF['CLUSTER']+1
     clusterDF['CLUSTER']  = clusterDF['CLUSTER'].astype(str)
     clusterDF=clusterDF.sort_values(['CLUSTER'])
-    if clusterSelected is not None:
-        if len(clusterSelected)>0:
-            dt=pd.merge(dt, clusterDF[~clusterDF['CLUSTER'].isin(list(map(str, clusterSelected)))],on=["COUNTRY_NAME"])
+
+    if clusterSelected is None:
+        if countries is None:
             countries=dt['COUNTRY_NAME'].unique()
-            campaings=dt['CAMPAIGN'].unique()
-            # st.write(dt)
-    if clusterSelected is not None:
-        if countries is not None and len(clusterSelected)==0:
-            dt=dt[dt["COUNTRY_NAME"].isin(orig['COUNTRY_NAME'].unique())]
-            dt=dt[dt["CAMPAIGN"].isin(orig['CAMPAIGN'].unique())]
-    else:
-        if countries is not None:
+            campaings=dt['CAMPAIGN'].unique()  
+        else:    
             dt=dt[dt["COUNTRY_NAME"].isin(countries)]
             dt=dt[dt["CAMPAIGN"].isin(campaings)]
+    elif len(clusterSelected)==0:
+        dt=dt[dt["COUNTRY_NAME"].isin(countries)]
+        dt=dt[dt["CAMPAIGN"].isin(campaings)]   
+    elif len(clusterSelected)>0:
+         dt=pd.merge(dt, clusterDF[~clusterDF['CLUSTER'].isin(list(map(str, clusterSelected)))],on=["COUNTRY_NAME"])
+         countries=dt['COUNTRY_NAME'].unique()
+         campaings=dt['CAMPAIGN'].unique()    
+
+         
     totalcost=getTotalCost(dt)
     totalcostOrig=getTotalCost(orig)
     compared=(1-((totalcost/totalcostOrig)))*100
@@ -101,16 +113,13 @@ def getPage(sess):
         getCard('SAVING: '+ str(round(compared,2))+'%',formatBigNumber(totalcostOrig - totalcost), 'fa fa-piggy-bank',True)  
         st.slider('Cluster Number',2,10,value=5,key='clusNum')
         clus=st.empty()
-        btn=st.empty()
+        st.button('Reset',on_click=reset())
+       
        
     with colR:
         st.subheader("Estimate Saving by Canceling Campaigns in Countries"  ) 
-        if clusterSelected is not None:
-            getCountrySelectionBox(orig,dt) 
-            getCampaignSelectionBox(orig,dt)
-        else:
-            getCountrySelectionBox(orig,dt) 
-            getCampaignSelectionBox(orig,dt)  
+        getCountrySelectionBox(orig,dt) 
+        getCampaignSelectionBox(orig,dt)  
         st.subheader("Clustering Countries by ER, CTR and Video Completion"  )       
         clus.multiselect('Exclude Cluster (Preempts All Other Exclusions):',np.unique(kmeans.labels_),key='clusterstore')
         
